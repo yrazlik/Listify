b173f21875c97dd90dbbc82646b02de2e4ab3886
yasin.razlik <yrazlik@gmail.com>
2026-03-22 14:21:01 +0300

    fixx

--- app/src/main/java/com/yrazlik/listify/CustomDialogFragment.java
+++ app/src/main/java/com/yrazlik/listify/CustomDialogFragment.java
@@ -1,10 +1,10 @@
 package com.yrazlik.listify;
 
 import android.app.Dialog;
-import android.app.DialogFragment;
+import androidx.fragment.app.DialogFragment;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
+import androidx.appcompat.app.AlertDialog;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
